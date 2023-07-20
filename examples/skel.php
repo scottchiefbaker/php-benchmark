452ae660b9f608dc2c1f191301a1bf579492493f
Scott Baker <scott.baker@directlink.coop>
2023-07-20 09:06:23 -0700

    Add skel.php to make future tests simpler

---
+++ examples/skel.php
@@ -0,0 +1,36 @@
+<?PHP
+
+require(dirname(__FILE__) . "/../benchmark.class.php");
+
+//////////////////////////////////////////////////////////////
+
+$bm = new benchmark;
+$bm->include_sample_output = 0;
+$bm->show_differences      = 0; // Show differences in the function return
+
+$a = function($x) {
+	// Test here
+};
+
+$b = function($x) {
+	// Test here
+};
+
+$c = function($str) {
+	// Test here
+};
+
+$d = function($i) {
+	// Test here
+};
+
+// Run the benchmark on each function
+$input = [2, 4, 6];
+
+$bm->time_this('test1', $a, $input);
+$bm->time_this('test2', $b, $input);
+$bm->time_this('test3', $c, $input);
+$bm->time_this('test4', $d, $input);
+
+// // Print out the summary of the benchmarks we've run
+$bm->summary();
